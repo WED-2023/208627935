f69b092a543f8f261a3ac796665c59ad85544e65
kobiza1 <kobiza@post.bgu.ac.il>
2024-05-14 23:42:58 +0300

    change the form to look better and to be responsive and center items in page

--- Jorden site/javaScript/jorden_js.js
+++ Jorden site/javaScript/jorden_js.js
@@ -4,7 +4,14 @@ form_open = false
 let variableDisplay = document.getElementById('variableDisplay');
 var my_envelope_div = document.getElementById('my_envelope');
 
+//emoji picker
+document.getElementById('emojiSelect').addEventListener('change', function() {
+  var emoji = this.value;
+  var messageTextarea = document.getElementById('message');
+  messageTextarea.value += emoji;
+});
 
+//open and close the contact us form
 my_envelope_div.addEventListener('click', function() {
   form_open = !form_open
   if (form_open){
@@ -15,7 +22,7 @@ my_envelope_div.addEventListener('click', function() {
 });
 
 variableDisplay.innerHTML = myVariable;
-
+// like and unlike the page
 function click_like_button(){
     liked = !liked;
     if (liked) {
@@ -27,3 +34,15 @@ function click_like_button(){
     }
     document.getElementById('likeCount').innerHTML = site_likes;
 }
+function send_contact_form(){
+  currentHtml = document.getElementById('contactForm').innerHTML
+  document.getElementById('contactForm').innerHTML = "Thank you for your message, we will answer you soon."
+  setTimeout(function() {
+      document.getElementById('contactForm').style.display = 'none';
+      document.getElementById('contactForm').innerHTML = currentHtml
+  }, 2000); 
+  
+}
+
+
+
